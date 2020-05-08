5418a4ce557347ca6b634204ecaaaaa509dc9bf0
Sergey Zwezdin <sergey@zwezdin.com>
2020-05-08 14:04:05 +0500

    feat: Config added

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-hexo.extend.generator.register('redirect', require('./lib/generator'));
+hexo.config.redirect = Object.assign(
+    {
+        enable: true,
+        layout: 'redirect'
+    },
+    hexo.config.redirect
+);
+
+if (hexo.config.redirect.enable) {
+    hexo.extend.generator.register('redirect', require('./lib/generator')(hexo));
+}
