3f781ec502a79fecb51517305425207201aabab3
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-03 19:38:39 +0700

    rename template

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-/* eslint-disable no-undef */
-hexo.config.redirect = Object.assign(
-  {
-    enable: true,
-    layout: 'redirect'
-  },
-  hexo.config.redirect
-);
+/* global hexo */
 
-if (hexo.config.redirect.enable) {
-  hexo.extend.generator.register('redirect', require('./lib/generator')(hexo));
+const generator = require('./lib/generator');
+
+if (typeof hexo !== 'undefined') {
+  hexo.config.redirect = Object.assign(
+    {
+      enable: false,
+      layout: 'hexo-generator-redirect'
+    },
+    hexo.config.redirect
+  );
+
+  if (hexo.config.redirect.enable) {
+    hexo.extend.generator.register('hexo-generator-redirect', generator(hexo));
+  }
+} else {
+  console.log('hexo-generator-redirect only for hexo plugin');
 }
