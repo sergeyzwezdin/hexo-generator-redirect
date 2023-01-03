48fbe7be89b8cab589666140daaa911dc44f645f
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-03 20:14:34 +0700

    remove auto generated files

---
+++ test.js
@@ -0,0 +1,7 @@
+const path = require('path');
+
+const testFolder = path.join(__dirname, 'test');
+const generatedTemplate = path.join(
+  testFolder,
+  'theme/next/layout/hexo-generator-redirect.njk'
+);
